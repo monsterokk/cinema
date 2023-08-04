fc88439a6c77646d2c48c5bba63cbf536ed56ebc
l.david.kovacs <l.david.kovacs@gmial.com>
2023-08-04 09:44:41 +0200

    project init

---
+++ src/main/java/com/example/gyakvizsgaangular/GyakvizsgaangularApplication.java
@@ -0,0 +1,13 @@
+package com.example.gyakvizsgaangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyakvizsgaangularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyakvizsgaangularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gyakvizsgaangular/GyakvizsgaangularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gyakvizsgaangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyakvizsgaangularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
